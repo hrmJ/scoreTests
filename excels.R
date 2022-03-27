39c74a891bd0ab1eba70a12b3cc22b554e40a59d
Juho Härme <juho.harme@gmail.com>
2022-03-27 21:49:00 +0300

    add some parsing to excels

--- excels.R
+++ excels.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+source("./utils.R")
 
 excelPath <- "Helmikuu 2022/"
 fullPath <- paste0("./", excelPath)
@@ -8,4 +9,15 @@ excelData <- list.files(path = fullPath, pattern = "*.xlsx") %>%
     show(path)
     return(readxl::read_xlsx(path))
   }) %>%
-  reduce(rbind)
+  reduce(rbind) %>%
+  mutate(goals = sapply(`Total Score`, getGoals)) %>%
+  mutate(parsedResult = sapply(`Total Score`, getGoals, asString = TRUE)) %>%
+  mutate(homeGoals = as.numeric(gsub("^(\\d+).*", "\\1", parsedResult))) %>%
+  mutate(awayGoals = as.numeric(gsub(".*-(\\d+)$", "\\1", parsedResult))) %>%
+  mutate(parsedTime = gsub("^(\\d+)\\w+", "\\1", Date)) %>%
+  mutate(parsedTime = gsub(" [A-Z]* \\(.*", "", parsedTime)) %>%
+  mutate(parsedTime = parse_datetime(parsedTime, "%d %b %Y, %H:%M")) %>%
+  mutate(htGoals = sapply(`1st Half Score`, getGoals)) %>%
+  mutate(HTparsedResult = sapply(`1st Half Score`, getGoals, asString = TRUE)) %>%
+  mutate(HThomeGoals = as.numeric(gsub("^(\\d+).*", "\\1", HTparsedResult))) %>%
+  mutate(HTawayGoals = as.numeric(gsub(".*-(\\d+)$", "\\1", HTparsedResult)))
